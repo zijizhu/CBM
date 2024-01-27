403e4d0eb3c4c946e38b67ee683bf155447bee59
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-01-27 16:19:40 +1100

    loss function tested

--- models/lm4cv.py
+++ models/lm4cv.py
@@ -1,23 +1,22 @@
 import torch
+import numpy as np
 from torch import nn
 
 
-def _mean_squared_mahalanobis(vecs: torch.Tensor, distribution: torch.Tensor):
-    '''Computes the mean of squared mahalanobis distances from a vector or a set of vectors to the distribution.
+def _mean_squared_mahalanobis(x: torch.Tensor, mu: torch.Tensor, sigma_inv):
+    '''Computes the mean of squared mahalanobis distances from a vector or a set of vectors to the distribution
+    with mean my and and inverse covariant matrix sigma_inv.
     Implementation from https://github.com/wangyu-ustc/LM4CV/blob/main/utils/train_utils.py#L263
     
     Args:
-        vec (Tensor[M, D]): a set of vector of length D.
+        x (Tensor[M, D]) or (Tensor[D]): a vector or a set of vector of length D.
         distribution (Tensor[N, D]) a matrix of N vectors of length D
     
     Returns:
         Tensor[]: a scaler tensor, which is the mahalanobis distance from vec to the distribution.
     '''
-
-    mu = torch.mean(distribution, dim=0)
-    sigma_inv = torch.linalg.inv(torch.cov(distribution.T))
-    vecs = vecs - mu.unsqueeze(0)
-    return torch.diag(vecs @ sigma_inv @ vecs.T).mean()
+    delta = x - mu.unsqueeze(0)
+    return torch.diag(delta @ sigma_inv @ delta.T).mean()
 
 
 class Stage1Criterion(nn.Module):
@@ -35,16 +34,18 @@ class Stage1Criterion(nn.Module):
         # Original implementation from https://github.com/wangyu-ustc/LM4CV/blob/main/utils/train_utils.py#L208
         # which is different to the one described in the paper.
         weights_norm = torch.linalg.norm(weights, dim=-1, keepdim=True)
-
         mu = torch.mean(full_concept_emb, dim=0)
-        sigma_inv = torch.inverse(torch.cov(full_concept_emb.T))
+        sigma_inv = torch.tensor(np.linalg.inv(torch.cov(full_concept_emb.T)))    # Using torch.inverse will have different result
+        # Alternate implementation: sigma_inv = torch.inverse(torch.cov(distribution.T))
+
         mean_distance = torch.stack([_mean_squared_mahalanobis(embed, mu, sigma_inv)
                                      for embed
                                      in full_concept_emb]).mean().to(outputs.device)
 
-        mahalanobis_loss = _mean_squared_mahalanobis(weights / weights_norm, mu, sigma_inv) 
+        mahalanobis_loss = _mean_squared_mahalanobis(weights / weights_norm, mu, sigma_inv)
+        mahalanobis_loss_scaled = (mahalanobis_loss - mean_distance) / (mean_distance ** self.division_power)
 
-        return xe_loss + (mahalanobis_loss - mean_distance) / (mean_distance ** self.division_power)
+        return xe_loss + torch.abs(mahalanobis_loss_scaled)
 
 
 class ImageEncoder(nn.Module):
